7488c98cbd4ec21b5f12ebd46d5f0ab82f21b2e1
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 00:52:13 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+let molengeek = ["coding15", "coding16", "coding17"]
+
+console.log(molengeek);
+
+molengeek.push("abdellah")
+
+console.log(molengeek);
+
+molengeek.splice(3, 1, "coding19");
+console.log(molengeek);
+
+molengeek.shift("coding15");
+console.log(molengeek);
